6520aa9e86296453fd00568fb72cdbf7e5070169
James E <4589250+Naught0@users.noreply.github.com>
2024-06-11 01:16:53 -0400

    Update link border color to match hover bg

--- components/hero-link.tsx
+++ components/hero-link.tsx
@@ -8,7 +8,7 @@ export const HeroLink = (
     <a
       {...props}
       className={cn(
-        "font-display border border-slate-300 bg-black px-10 py-3 text-3xl font-bold shadow-black transition-all hover:translate-x-2 hover:bg-primary-foreground hover:text-primary hover:drop-shadow-md sm:text-4xl",
+        "font-display border border-primary-foreground bg-black px-10 py-3 text-3xl font-bold shadow-black transition-all hover:translate-x-2 hover:bg-primary-foreground hover:text-primary hover:drop-shadow-md sm:text-4xl",
         props.className,
       )}
     >
