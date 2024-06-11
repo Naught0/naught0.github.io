675b1b1160902fbc6826b2672fcb1bee55c73744
James E <4589250+Naught0@users.noreply.github.com>
2024-06-11 02:25:33 -0400

    Window isn't defined and idk why

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -6,7 +6,7 @@ import { HoverScaleLink } from "./hover-scale-link";
 export function NavLink(props: PropsWithChildren & { href: string }) {
   return (
     <HoverScaleLink
-      className={`font-display inline-flex items-center gap-1 text-base font-black sm:text-xl ${props.href === window.location.pathname ? "underline underline-offset-8" : "text-primary-foreground"}`}
+      className="font-display inline-flex items-center gap-1 text-base font-black sm:text-xl"
       href={props.href}
     >
       {props.children}
